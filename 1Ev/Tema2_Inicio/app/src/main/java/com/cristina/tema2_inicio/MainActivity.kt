a2d9f0738c8a9ff25ea7558226ee2931d25f6ab2
Victorgonzalez-ces <victor.gonzalez@cesjuanpablosegundo.es>
2023-10-04 14:32:03 +0200

    update

--- 1Ev/Tema2_Inicio/app/src/main/java/com/cristina/tema2_inicio/MainActivity.kt
+++ 1Ev/Tema2_Inicio/app/src/main/java/com/cristina/tema2_inicio/MainActivity.kt
@@ -2,10 +2,19 @@ package com.cristina.tema2_inicio
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var boton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        boton = findViewById(R.id.boton_pulsar)
+
+        boton.setOnClickListener{
+            
+        }
     }
 }
