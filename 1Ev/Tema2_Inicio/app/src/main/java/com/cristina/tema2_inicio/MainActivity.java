b56ddf56a8bacac32f867fe27c72162c78df6581
Victorgonzalez-ces <victor.gonzalez@cesjuanpablosegundo.es>
2023-10-17 14:05:46 +0200

    update

---
+++ 1Ev/Tema2_Inicio/app/src/main/java/com/cristina/tema2_inicio/MainActivity.java
@@ -0,0 +1,6 @@
+package com.cristina.tema2_inicio;
+
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+}
--- 1Ev/Tema2_Inicio/app/src/main/java/com/cristina/tema2_inicio/MainActivity.kt
+++ 1Ev/Tema2_Inicio/app/src/main/java/com/cristina/tema2_inicio/MainActivity.kt
@@ -1,20 +1,38 @@
-package com.cristina.tema2_inicio
+package com.example.t2_inicio
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.widget.Button
+import com.cristina.tema2_inicio.R
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var boton: Button
+    lateinit var boton: Button;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        boton = findViewById(R.id.boton_pulsar)
+        instancias()
+        acciones()
 
-        boton.setOnClickListener{
 
+    }
+
+    private fun acciones() {
+        boton.setOnClickListener {
+            var notificion = Snackbar.make(it, R.string.text_superada, Snackbar.LENGTH_INDEFINITE)
+            notificion.setAction(R.string.text_pulsar){
+                // ocultar la notificacion
+                notificion.dismiss()
+
+            }
+            notificion.show()
         }
+    }
+
+    private fun instancias() {
+        boton = findViewById(R.id.boton_pulsar)
     }
 }
