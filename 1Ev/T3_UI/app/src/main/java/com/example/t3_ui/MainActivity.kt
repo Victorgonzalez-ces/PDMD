698202dae16b0b23cd05df769a85742dc911609a
Victorgonzalez-ces <victor.gonzalez@cesjuanpablosegundo.es>
2023-10-31 14:11:19 +0100

    update

---
+++ 1Ev/T3_UI/app/src/main/java/com/example/t3_ui/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.t3_ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.example.t3_ui.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.botonLimpiar.setOnClickListener(this)
+        binding.botonRegistrar.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            binding.botonLimpiar.id->{}
+            binding.botonRegistrar.id->{}
+        }
+    }
+}
