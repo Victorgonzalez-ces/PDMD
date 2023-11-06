f3982a1ca3b68b63796ae0e29efa872f42945d84
Victorgonzalez-ces <victor.gonzalez@cesjuanpablosegundo.es>
2023-11-06 10:22:16 +0100

    update

--- 1Ev/T3_UI/app/src/main/java/com/example/t3_ui/MainActivity.kt
+++ 1Ev/T3_UI/app/src/main/java/com/example/t3_ui/MainActivity.kt
@@ -3,8 +3,13 @@ package com.example.t3_ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
 import android.widget.Button
+import android.widget.RadioButton
+import android.widget.Toast
 import com.example.t3_ui.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationBarView
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -17,12 +22,32 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         setContentView(binding.root)
         binding.botonLimpiar.setOnClickListener(this)
         binding.botonRegistrar.setOnClickListener(this)
+        binding.grupoTipo.setOnCheckedChangeListener { _, checkedId ->
+            val seleccion = (this.findViewById<RadioButton>(checkedId)).text
+            Snackbar.make(binding.root, seleccion, Snackbar.LENGTH_SHORT).show()
+        }
+        binding.spinnerPerfil.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                //binding.spinnerPerfil.selectedItem
+                val seleccionado = parent?.adapter?.getItem(position).toString()
+                Snackbar.make(binding.root, seleccionado, Snackbar.LENGTH_SHORT).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+        }
     }
 
     override fun onClick(v: View?) {
         when(v!!.id){
             binding.botonLimpiar.id->{}
-            binding.botonRegistrar.id->{}
+            binding.botonRegistrar.id->{
+                val seleccionado = binding.spinnerPerfil.selectedItem.toString()
+                val seleccion = findViewById(checkedRadioButton).text
+                Snackbar.make(binding.root, seleccionado, Snackbar.LENGTH_SHORT).show()
+                Toast.makeText(applicationContext, seleccion, Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
