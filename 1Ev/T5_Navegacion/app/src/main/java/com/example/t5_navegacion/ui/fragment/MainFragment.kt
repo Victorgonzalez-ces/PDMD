8bb67a6d17b52189cb511e800250f50cc8b43050
Victorgonzalez-ces <victor.gonzalez@cesjuanpablosegundo.es>
2024-01-12 11:16:36 +0100

    update

---
+++ 1Ev/T5_Navegacion/app/src/main/java/com/example/t5_navegacion/ui/fragment/MainFragment.kt
@@ -0,0 +1,40 @@
+package com.example.t5_navegacion.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.t5_navegacion.databinding.FragmentMainBinding
+import com.example.t5_navegacion.databinding.FragmentSigninBinding
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class MainFragment : Fragment() {
+
+    private var _binding: FragmentMainBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ 1Ev/T5_Navegacion/app/src/main/java/com/example/t5_navegacion/ui/fragment/SigninFragment.kt
@@ -0,0 +1,39 @@
+package com.example.t5_navegacion.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.t5_navegacion.databinding.FragmentSigninBinding
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class SigninFragment : Fragment() {
+
+    private var _binding: FragmentSigninBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentSigninBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
